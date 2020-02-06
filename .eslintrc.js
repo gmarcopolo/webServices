b27acfe69b16ed414071fc28f27f68f614055d7f
Marco Polo <g.marcopolo@gmail.com>
2020-02-06 17:02:10 -0500

    style (lint): prettified

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,13 @@ module.exports = {
     commonjs: true,
     es6: true
   },
-  extends: ["airbnb-base"],
+  extends: ['airbnb-base'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaVersion: 2018
   },
-  rules: {
-    "linebreak-style": 0
-  }
+  rules: {}
 };
