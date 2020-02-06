a253636692cc7513ac67692fba69a247dd721ada
Marco Polo <g.marcopolo@gmail.com>
2020-02-06 10:34:40 -0500

    add cli table to make more visible the host url

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const Table = require('cli-table');
+
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 const port = process.env.PORT || 3000;
@@ -22,5 +24,11 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Running on port  ${port}`);
+  const Table = require('cli-table');
+  const table = new Table({
+    style: { head: ['blue'], border: ['blue'] }
+  });
+
+  table.push({ 'Running on': `http://localhost:${port}` });
+  console.log(table.toString());
 });
